2d814947c811f4c7badde77de1fc3ae0d2c0085b
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-25 19:23:18 +0600

    Complete
    Finished the desired work. Scores now lock at max point, which can be defined by the user by simply using the input field. The winning score is also highlighted in green.

--- scorekeeper.js
+++ scorekeeper.js
@@ -5,6 +5,9 @@ let resetButton = document.querySelector("#reset");
 let p1Display = document.querySelector("#p1Display");
 let p2Display = document.querySelector("#p2Display");
 
+let numInput = document.querySelector("input[type = 'number']");
+let winningScoreDisplay = document.querySelector("p span");
+
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
@@ -14,6 +17,7 @@ p1Button.addEventListener("click", function(){
 	if(!gameOver){
 		p1Score++;
 		if(p1Score === winningScore){
+			p1Display.classList.add("text-success");
 			gameOver = true;
 		}
 		p1Display.textContent = p1Score;
@@ -24,8 +28,31 @@ p2Button.addEventListener("click", function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score === winningScore){
+			p2Display.classList.add("text-success");
 			gameOver = true;
 		}
 		p2Display.textContent = p2Score;
 	}
 })
+
+resetButton.addEventListener("click", function(){
+	reset();
+})
+
+numInput.addEventListener("click", function(){
+	if(this.value > 0){
+		winningScoreDisplay.textContent = this.value;
+		winningScore = Number(numInput.value);
+		reset();
+	}
+})
+
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("text-success");
+	p2Display.classList.remove("text-success");
+	gameOver = false;
+}
