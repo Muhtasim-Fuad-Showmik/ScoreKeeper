1a9ffdcbd7a4559f9f019d8d517767daf53894d9
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-25 18:52:39 +0600

    Added Score Increment Functionalities
    Allowed for the buttons to work such that the score increases one by one on each click unless the default winning score of 5 has been reached. At which point, the score no longer updates no matter how many times the buttons are clicked from then on.

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,1 +1,31 @@
-alert("Connected!");
+let p1Button = document.querySelector("#p1");
+let p2Button = document.querySelector("#p2");
+let resetButton = document.querySelector("#reset");
+
+let p1Display = document.querySelector("#p1Display");
+let p2Display = document.querySelector("#p2Display");
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let gameOver = false;
+
+p1Button.addEventListener("click", function(){
+	if(!gameOver){
+		p1Score++;
+		if(p1Score === winningScore){
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
+})
+
+p2Button.addEventListener("click", function(){
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === winningScore){
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;
+	}
+})
